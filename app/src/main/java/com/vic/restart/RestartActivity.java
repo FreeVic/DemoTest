bb98fb69a535fd23385528b101a1bc6f33ea0858
FreeVic <freeverse@qq.com>
2017-12-07 17:13:21 +0800

    Revert "add freeline"
    This reverts commit 9d09074052f8446d8c3e984064faf263c8779e95.

--- app/src/main/java/com/vic/restart/RestartActivity.java
+++ app/src/main/java/com/vic/restart/RestartActivity.java
@@ -57,8 +57,7 @@ public class RestartActivity extends BaseActivity {
         System.out.println("status height:" + statusBarHeight);
         interceptHyperLink(tvTest);
         System.out.println("buildConfigField="+BuildConfig.example);
-        System.out.println("testfreeline="+BuildConfig.example);
-//        System.out.println("testfreeline2="+BuildConfig.example);
+
     }
 
     /**
